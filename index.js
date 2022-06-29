a1e44d576264d8407f4a44417dbebdfe42d868ab
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-06-29 01:38:26 -0300

    suma de arrays

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+const hombres = ["matias", "esteban", "diego", "gabriel", "gonzalo"];
+const mujeres = ["joana", "maria", "nancy", "sabrina", "rocio",];
+hombres.push ("jony");
+mujeres.push("carla");
+let TodosJuntos = hombres + mujeres;
+console.log(TodosJuntos);
+
+console.log( hombres.join (" "));
+console.log( mujeres.join ("_"));
+
